57f73e1e2a0efac5a9226e9dccb7925035bc8866
MarjonaDavlyatova <marjona.davlyatova.h@gmail.com>
2024-07-09 17:07:13 +0500

    m

---
+++ config.py
@@ -0,0 +1,4 @@
+iam_token = "t1.9euelZqdl4uKmI6MnpHJy8bHl5zIzO3rnpWaicyOk5ebycbHyYvPz8-Mnc_l8_dzWUtL-e8xIEUT_d3z9zMISUv57zEgRRP9zef1656Vmpyek5aVipvGmcuPnsyVk56K7_zF656Vmpyek5aVipvGmcuPnsyVk56K.O_DhBZinOPVGf5UvuHzglcukfjF5CGq3LVCR8bydXwksfUH7NxT1XHSb8-1qEHuQPbsYlQ78eGsNCNzcaq8JCw"
+catalog_id = "b1gonr4969r16d19ckpp"
+url_1 = "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync"
+url_2 = "https://llm.api.cloud.yandex.net:443/operations"
